8a7084ab6d2db711207b626cd7b1703e7bd366eb
3 Oliveira <Marco>
2023-03-07 15:39:15 -0300

    att profile + hot toast

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,14 +1,23 @@
 import { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { api } from "../../api/api";
-import { Card, CardHeader, CardBody,  CardFooter, Input, Button, Tooltip, Typography } from "@material-tailwind/react";
-
-
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Input,
+  Button,
+  Typography,
+} from "@material-tailwind/react";
+import { toast } from "react-hot-toast";
 
 export function Profile() {
-
   const [user, setUser] = useState({ name: "", email: "" });
-  const [reload, setReload] = useState(false)
+  const [reload, setReload] = useState(false);
+  const [img, setImg] = useState("");
+
+  const complete = () => toast.success("Alterações realizadas!");
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function fetchUser() {
@@ -23,79 +32,84 @@ export function Profile() {
     setUser({ ...user, [e.target.name]: e.target.value });
   }
 
+  function handleImage(e) {
+    setImg(e.target.files[0]);
+  }
+
+  async function handleUpload() {
+    try {
+      const uploadData = new FormData();
+      uploadData.append("picture", img);
+
+      const response = await api.post("/uploadImage", uploadData);
+
+      return response.data.url;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   async function handleSubmit(e) {
     try {
       e.preventDefault();
+      const imgURL = await handleUpload();
+      await api.put(`/user`, { ...user, image: imgURL });
 
-      await api.put(`/user`, user);
-
-      setReload(!reload)
-
+      setReload(!reload);
+      navigate("/");
     } catch (err) {
       console.log(err);
     }
   }
 
-
- 
   return (
-    <div className="flex justify-center space-x-40 mt-5 ">
-      <div>
-      <Card className="w-96">
-      <CardHeader floated={false} className="h-80">
-        <img src={user.image} alt="profile-picture" />
-      </CardHeader>
-      <CardBody className="text-center">
-        <Typography variant="h4" color="blue-gray" className="mb-2">
-         {user.name}
-        </Typography>
-        <Typography color="blue" className="font-medium" textGradient>
-          {user.email}
-        </Typography>
-      </CardBody>
-      <CardFooter className="flex justify-center gap-7 pt-2">
-        <Tooltip content="Like">
-          <Typography
-            as="a"
-            href="#facebook"
-            variant="lead"
-            color="blue"
-            textGradient
-          >
-            <i className="fab fa-facebook" />
-          </Typography>
-        </Tooltip>
-        <Tooltip content="Follow">
-          <Typography
-            as="a"
-            href="#twitter"
-            variant="lead"
-            color="light-blue"
-            textGradient
-          >
-            <i className="fab fa-twitter" />
-          </Typography>
-        </Tooltip>
-        <Tooltip content="Follow">
-          <Typography
-            as="a"
-            href="#instagram"
-            variant="lead"
-            color="purple"
-            textGradient
-          >
-            <i className="fab fa-instagram" />
-          </Typography>
-        </Tooltip>
-      </CardFooter>
-    </Card>
+    <div className="flex justify-center align space-x-20 mt-5">
+      <div className="w-2/5">
+        <Card className="w-96 ml-8 ">
+          <CardHeader floated={false} className="h-80">
+            <img
+              src={user.image}
+              alt="profile-picture"
+              className="w-full h-full"
+            />
+          </CardHeader>
+          <CardBody className="text-center">
+            <Typography
+              variant="h4"
+              color="blue-gray"
+              className="mb-2"
+              textGradient
+            >
+              {user.name}
+            </Typography>
+            <Typography color="blue" className="font-medium" textGradient>
+              {user.email}
+            </Typography>
+          </CardBody>
+        </Card>
         <Link to="/orders">
-       <p> Minhas ordens</p>
+          <Typography className="text-blue-400 hover:text-blue-800 mt-5">
+            Minhas ordens
+          </Typography>
         </Link>
       </div>
-      <div>
-        <form onSubmit={handleSubmit} className="mt-8 mb-2 sm:w-96">
+      <div className="w-3/5 pr-9">
+        <form onSubmit={handleSubmit} className="mt-8 mb-2 sm:w-full">
           <div className="mb-4 flex flex-col gap-6">
+            <div className="border-dashed border-2 p-3">
+              <label htmlFor="image">
+                <Typography className="font-semibold text-gray-400 mb-2">
+                  Editar foto
+                </Typography>
+              </label>
+              <input
+                type="file"
+                label="Foto usuário"
+                id="image"
+                name="image"
+                onChange={handleImage}
+              />
+            </div>
             <Input
               variant="outlined"
               className=""
@@ -103,7 +117,7 @@ export function Profile() {
               label="Usuário"
               id="formUser"
               name="userName"
-              value={user.userName || ''}
+              value={user.userName || ""}
               required
               onChange={handleChange}
             />
@@ -113,7 +127,7 @@ export function Profile() {
               size="lg"
               label="Nome completo"
               name="name"
-              value={user.name || ''}
+              value={user.name || ""}
               required
               onChange={handleChange}
             />
@@ -123,7 +137,7 @@ export function Profile() {
               size="lg"
               label="Rua"
               name="street"
-              value={user.street || ''}
+              value={user.street || ""}
               type="text"
               required
               onChange={handleChange}
@@ -134,7 +148,7 @@ export function Profile() {
               size="lg"
               label="Número"
               name="houseNumber"
-              value={user.houseNumber || ''}
+              value={user.houseNumber || ""}
               type="number"
               required
               onChange={handleChange}
@@ -145,7 +159,7 @@ export function Profile() {
               size="lg"
               label="Complemento"
               name="apartmentNumber"
-              value={user.apartmentNumber || ''}
+              value={user.apartmentNumber || ""}
               type="text"
               onChange={handleChange}
             />
@@ -155,7 +169,7 @@ export function Profile() {
               size="lg"
               label="Cidade"
               name="city"
-              value={user.city || ''}
+              value={user.city || ""}
               type="text"
               onChange={handleChange}
             />
@@ -166,7 +180,7 @@ export function Profile() {
               size="lg"
               label="Bairro"
               name="neighborhood"
-              value={user.neighborhood || ''}
+              value={user.neighborhood || ""}
               type="text"
               onChange={handleChange}
             />
@@ -177,7 +191,7 @@ export function Profile() {
               size="lg"
               label="Estado"
               name="state"
-              value={user.state || ''}
+              value={user.state || ""}
               type="text"
               maxLength="5"
               list="estados"
@@ -190,7 +204,7 @@ export function Profile() {
               size="lg"
               label="CEP"
               name="CEP"
-              value={user.CEP || ''}
+              value={user.CEP || ""}
               type="text"
               required
               maxLength="9"
@@ -205,22 +219,23 @@ export function Profile() {
               name="email"
               type="email"
               required
-              value={user.email || ''}
+              value={user.email || ""}
               onChange={handleChange}
             />
-            {/* <Input
+            <Input
               variant="outlined"
               className=""
               id="formPassword"
               label="Password"
               name="password"
               type="password"
-              required
-              value={user.password || ''}
+              value={user.password || ""}
               onChange={handleChange}
               size="lg"
-            /> */}
-            <Button color="yellow" type="submit">Confirmar edição</Button>
+            />
+            <Button color="yellow" type="submit" onClick={complete}>
+              Confirmar edição
+            </Button>
           </div>
         </form>
       </div>
