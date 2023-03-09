70a067fcb5d2b7b3a479fc5ab07117980b73407b
3 Oliveira <Marco>
2023-03-09 18:52:59 -0300

    att load orders, productuser

--- src/pages/Orders/index.jsx
+++ src/pages/Orders/index.jsx
@@ -16,6 +16,8 @@ import "slick-carousel/slick/slick-theme.css";
 
 export function Orders() {
   const [orders, setOrders] = useState([]);
+  const [load, setLoad] = useState(true);
+
   const formatter = new Intl.NumberFormat("pt-BR", {
     style: "currency",
     currency: "BRL",
@@ -26,6 +28,7 @@ export function Orders() {
         const response = await api.get(`/order/get`);
 
         setOrders([...response.data]);
+        setLoad(false);
       } catch (err) {
         console.log(err);
       }
@@ -38,89 +41,91 @@ export function Orders() {
       <div>
         <h1 className="mt-3 text-xl font-bold">Ordens</h1>
       </div>
-      <div className="container flex mx-auto bg-gray-300 rounded-xl p-8 m-10 gap-7">
-        <Slider
-          dots={true}
-          infinite={false}
-          speed={500}
-          slidesToShow={3}
-          slidesToScroll={3}
-          className="w-full gap-7"
-        >
-          {orders.map((currentOrder) => {
-            return (
-              <Link to={`/order-details/${currentOrder._id}`}>
-                <Card
-                  color="gray"
-                  variant="gradient"
-                  className="w-full max-w-[20rem] p-8 transition-scale duration-500 ease-in-out transform hover:scale-110 rounded-lg"
-                  key={currentOrder._id}
-                >
-                  <CardHeader
-                    floated={false}
-                    shadow={false}
-                    color="transparent"
-                    className="m-0 mb-8 rounded-none border-b border-white/10 pb-8 text-center "
+      {!load && (
+        <div className="container flex mx-auto bg-gray-300 rounded-xl p-8 m-10 gap-7">
+          <Slider
+            dots={true}
+            infinite={false}
+            speed={500}
+            slidesToShow={3}
+            slidesToScroll={3}
+            className="w-full gap-7"
+          >
+            {orders.map((currentOrder) => {
+              return (
+                <Link to={`/order-details/${currentOrder._id}`}>
+                  <Card
+                    color="gray"
+                    variant="gradient"
+                    className="w-full max-w-[20rem] p-8 transition-scale duration-500 ease-in-out transform hover:scale-110 rounded-lg"
+                    key={currentOrder._id}
                   >
-                    <Typography
-                      variant="large"
-                      color="white"
-                      className="font-normal uppercase"
+                    <CardHeader
+                      floated={false}
+                      shadow={false}
+                      color="transparent"
+                      className="m-0 mb-8 rounded-none border-b border-white/10 pb-8 text-center "
                     >
-                      {`${currentOrder.productId.productName}`}
-                    </Typography>
-                    <h1 className="mt-6 flex justify-center gap-1 text-sm font-bold  bg-white">
-                      {formatter.format(currentOrder.totalPrice)}
-                    </h1>
-                  </CardHeader>
-                  <CardBody className="p-0">
-                    <ul className="flex flex-col gap-4">
-                      <li className="flex items-center gap-4">
-                        <span className="rounded-full border border-white/20 bg-white/20 p-1">
-                          <CheckIcon strokeWidth={2} className="h-3 w-3" />
-                        </span>
-                        <Typography className="text-sm">
-                          Pagamento: {currentOrder.payment}
-                        </Typography>
-                      </li>
-                      <li className="flex items-center gap-4">
-                        <span className="rounded-full border border-white/20 bg-white/20 p-1">
-                          <CheckIcon strokeWidth={2} className="h-3 w-3" />
-                        </span>
-                        <Typography className="text-sm">
-                          Quantidade: {currentOrder.quantity}
-                        </Typography>
-                      </li>
-                      <li className="flex items-center gap-4">
-                        <span className="rounded-full border border-white/20 bg-white/20 p-1">
-                          <CheckIcon strokeWidth={2} className="h-3 w-3" />
-                        </span>
-                        <Typography className="text-sm">
-                          Status: {currentOrder.status}
-                        </Typography>
-                      </li>
-                      <li className="flex items-center gap-4">
-                        <span className="rounded-full border border-white/20 bg-white/20 p-1">
-                          <CheckIcon strokeWidth={2} className="h-3 w-3" />
-                        </span>
-                        <Typography className="text-sm">
-                          Pedido efetuado:{" "}
-                          {new Date(
-                            currentOrder.createdAt
-                          ).toLocaleDateString()}
-                        </Typography>
-                      </li>
-                    </ul>
-                  </CardBody>
-                  <CardFooter className="mt-12 p-0">
-                    <span>Clique para detalhes</span>
-                  </CardFooter>
-                </Card>
-              </Link>
-            );
-          })}
-        </Slider>
-      </div>
+                      <Typography
+                        variant="large"
+                        color="white"
+                        className="font-normal uppercase"
+                      >
+                        {`${currentOrder.productId.productName}`}
+                      </Typography>
+                      <h1 className="mt-6 flex justify-center gap-1 text-sm font-bold  bg-white">
+                        {formatter.format(currentOrder.totalPrice)}
+                      </h1>
+                    </CardHeader>
+                    <CardBody className="p-0">
+                      <ul className="flex flex-col gap-4">
+                        <li className="flex items-center gap-4">
+                          <span className="rounded-full border border-white/20 bg-white/20 p-1">
+                            <CheckIcon strokeWidth={2} className="h-3 w-3" />
+                          </span>
+                          <Typography className="text-sm">
+                            Pagamento: {currentOrder.payment}
+                          </Typography>
+                        </li>
+                        <li className="flex items-center gap-4">
+                          <span className="rounded-full border border-white/20 bg-white/20 p-1">
+                            <CheckIcon strokeWidth={2} className="h-3 w-3" />
+                          </span>
+                          <Typography className="text-sm">
+                            Quantidade: {currentOrder.quantity}
+                          </Typography>
+                        </li>
+                        <li className="flex items-center gap-4">
+                          <span className="rounded-full border border-white/20 bg-white/20 p-1">
+                            <CheckIcon strokeWidth={2} className="h-3 w-3" />
+                          </span>
+                          <Typography className="text-sm">
+                            Status: {currentOrder.status}
+                          </Typography>
+                        </li>
+                        <li className="flex items-center gap-4">
+                          <span className="rounded-full border border-white/20 bg-white/20 p-1">
+                            <CheckIcon strokeWidth={2} className="h-3 w-3" />
+                          </span>
+                          <Typography className="text-sm">
+                            Pedido efetuado:{" "}
+                            {new Date(
+                              currentOrder.createdAt
+                            ).toLocaleDateString()}
+                          </Typography>
+                        </li>
+                      </ul>
+                    </CardBody>
+                    <CardFooter className="mt-12 p-0">
+                      <span>Clique para detalhes</span>
+                    </CardFooter>
+                  </Card>
+                </Link>
+              );
+            })}
+          </Slider>
+        </div>
+      )}
     </div>
   );
 }
--- src/pages/ProductsUser/index.jsx
+++ src/pages/ProductsUser/index.jsx
@@ -16,13 +16,14 @@ import "slick-carousel/slick/slick-theme.css";
 
 export function ProductsUser() {
   const [products, setProducts] = useState([]);
-
+  const [load, setLoad] = useState(true);
   useEffect(() => {
     async function fetchProducts() {
       try {
         const response = await api.get("/product/products-user");
 
         setProducts([...response.data]);
+        setLoad(false);
       } catch (err) {
         console.log(err);
       }
@@ -33,68 +34,72 @@ export function ProductsUser() {
   return (
     <div>
       <h1>Meus produtos a venda</h1>
-      <div className="flex mx-auto bg-gray-200 rounded-xl p-8 m-10">
-        <Slider
-          dots={true}
-          infinite={true}
-          speed={500}
-          slidesToShow={3}
-          slidesToScroll={3}
-          className="w-full h-full"
-        >
-          {products.map((currentProduct) => {
-            return (
-              <Card
-                className="max-w-[18rem] max-h-[20rem] overflow-hidden"
-                key={currentProduct._id}
-              >
-                <CardHeader
-                  floated={false}
-                  shadow={false}
-                  color="transparent"
-                  className="m-0 rounded-none flex justify-center"
-                >
-                  <img
-                    src={currentProduct.image}
-                    alt={currentProduct.name}
-                    className="h-28 pt-2"
-                  />
-                </CardHeader>
-                <CardBody className="pb-1 pt-1">
-                  <Typography variant="h6" color="blue-gray">
-                    {currentProduct.productName}
-                  </Typography>
-                  <Typography
-                    variant="lead"
-                    color="gray"
-                    className="mt-2 text-sm font-normal line-clamp-2"
+      {!load && (
+        <>
+          <div className="flex mx-auto bg-gray-200 rounded-xl p-8 m-10">
+            <Slider
+              dots={true}
+              infinite={true}
+              speed={500}
+              slidesToShow={3}
+              slidesToScroll={3}
+              className="w-full h-full"
+            >
+              {products.map((currentProduct) => {
+                return (
+                  <Card
+                    className="max-w-[18rem] max-h-[20rem] overflow-hidden"
+                    key={currentProduct._id}
                   >
-                    {currentProduct.description}
-                  </Typography>
-                </CardBody>
-                <CardFooter className="flex items-center justify-between pt-1">
-                  <div className="flex items-center -space-x-3">
-                    <Tooltip content={currentProduct.sellerId.name}>
-                      <Avatar
-                        size="sm"
-                        variant="circular"
-                        alt={currentProduct.sellerId.name}
-                        src={currentProduct.sellerId.image}
-                        className="border-2 border-white hover:z-10"
+                    <CardHeader
+                      floated={false}
+                      shadow={false}
+                      color="transparent"
+                      className="m-0 rounded-none flex justify-center"
+                    >
+                      <img
+                        src={currentProduct.image}
+                        alt={currentProduct.name}
+                        className="h-28 pt-2"
                       />
-                    </Tooltip>
-                  </div>
-                  <Link to={`/product-details/${currentProduct._id}`}>
-                    <Typography className="font-normal text-blue-400 hover:underline hover:text-blue-800">
-                      Saiba mais!
-                    </Typography>
-                  </Link>
-                </CardFooter>
-              </Card>
-            );
-          })}
-        </Slider>
-      </div>
+                    </CardHeader>
+                    <CardBody className="pb-1 pt-1">
+                      <Typography variant="h6" color="blue-gray">
+                        {currentProduct.productName}
+                      </Typography>
+                      <Typography
+                        variant="lead"
+                        color="gray"
+                        className="mt-2 text-sm font-normal line-clamp-2"
+                      >
+                        {currentProduct.description}
+                      </Typography>
+                    </CardBody>
+                    <CardFooter className="flex items-center justify-between pt-1">
+                      <div className="flex items-center -space-x-3">
+                        <Tooltip content={currentProduct.sellerId.name}>
+                          <Avatar
+                            size="sm"
+                            variant="circular"
+                            alt={currentProduct.sellerId.name}
+                            src={currentProduct.sellerId.image}
+                            className="border-2 border-white hover:z-10"
+                          />
+                        </Tooltip>
+                      </div>
+                      <Link to={`/product-details/${currentProduct._id}`}>
+                        <Typography className="font-normal text-blue-400 hover:underline hover:text-blue-800">
+                          Saiba mais!
+                        </Typography>
+                      </Link>
+                    </CardFooter>
+                  </Card>
+                );
+              })}
+            </Slider>
+          </div>
+        </>
+      )}
     </div>
   );
 }
