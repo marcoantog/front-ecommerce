91b7b82dde4e8a4a8fd0f965d604cdeceaee994d
bebossi <bernardobossi77@gmail.com>
2023-03-10 09:55:02 -0300

    removendo item da wishlist

--- src/pages/WishList/index.jsx
+++ src/pages/WishList/index.jsx
@@ -1,15 +1,26 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { WishListContext } from "../../context/WishListContext";
 import { Link } from "react-router-dom";
 
 export function WishList() {
-  const { wishList } = useContext(WishListContext);
+  const { wishList, setWishList } = useContext(WishListContext);
 
-  function removeFromWishList() {
-    localStorage.removeItem("wishList", JSON.stringify(wishList));
+  function removeFromWishList(productId) {
+    const updatedWishList = wishList.filter(
+      (product) => product._id !== productId
+    );
+    localStorage.setItem("wishList", JSON.stringify(updatedWishList));
+    setWishList(updatedWishList);
   }
 
-  console.log(wishList);
+  useEffect(() => {
+    try {
+      removeFromWishList(wishList._id);
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
   return (
     <div>
       <div>
@@ -19,38 +30,52 @@ export function WishList() {
       <div className="grid w-full flex-col justify-center">
         {wishList.map((product, i) => {
           return (
-            <Link key={product._id} to={`/product-details/${product._id}`}>
-              <div className="w-6xl mt-3 bg-white rounded-lg overflow-hidden shadow-md md:flex">
-                <div className="md:flex-shrink-0">
-                  <img
-                    className="h-64 w-full object-cover md:w-64"
-                    src={product.image}
-                    alt={product.productName}
-                  />
+            <div
+              key={product._id}
+              className="w-6xl mt-3 bg-white rounded-lg overflow-hidden shadow-md md:flex"
+            >
+              <div className="md:flex-shrink-0">
+                <img
+                  className="h-64 w-full object-cover md:w-64"
+                  src={product.image}
+                  alt={product.productName}
+                />
+              </div>
+              <div className="p-5  md:flex-1">
+                <div className=" flex justify-around gap-40 uppercase tracking-wide text-3xl text-indigo-500 font-bold">
+                  <p>{product.productName}</p>
+                  <span className="text-green-500 font-semibold text-3xl">
+                    R${product.price}
+                  </span>
                 </div>
-                <div className="p-5  md:flex-1">
-                  <div className=" flex justify-around gap-40 uppercase tracking-wide text-3xl text-indigo-500 font-bold">
-                    <p>{product.productName}</p>
-                    <span className="text-green-500 font-semibold text-3xl">
-                      R${product.price}
+                <div>
+                  <div className="flex justify-between text-xl mt-10">
+                    <span className="font-bold ml-4">
+                      Estado: {`${product.isUsed ? " usado" : " novo"}`}
                     </span>
-                  </div>
-                  <div>
-                    <div className="flex justify-between text-xl mt-10">
-                      <span className="font-bold ml-4">
-                        Estado: {`${product.isUsed ? " usado" : " novo"}`}
-                      </span>
-                      <p className="font-bold mr-4 ">
-                        Em estoque: {product.quantity}
-                      </p>
-                    </div>
-                    <p className="mt-8 text-gray-600 text-lg">
-                      {product.description}
+                    <p className="font-bold mr-4 ">
+                      Em estoque: {product.quantity}
                     </p>
                   </div>
+                  <p className="mt-8 text-gray-600 text-lg">
+                    {product.description}
+                  </p>
+                </div>
+                <div className="flex justify-around mt-3">
+                  <Link to={`/product-details/${product._id}`}>
+                    <button className="bg-indigo-500 ml-4 border rounded-lg px-4 py-2 hover:border-indigo-500">
+                      ver mais
+                    </button>
+                  </Link>
+                  <button
+                    className="bg-red-400 ml-4 border rounded-lg px-8 py-2 hover:border-indigo-500"
+                    onClick={() => removeFromWishList(product._id)}
+                  >
+                    remover
+                  </button>
                 </div>
               </div>
-            </Link>
+            </div>
           );
         })}
       </div>
